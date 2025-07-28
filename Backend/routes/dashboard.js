b95ca1e5421968595ff66f9a0579246f089948bd
Hemendra Kumar <hem.pachahara@gmail.com>
2025-07-28 18:44:57 +0530

    backend separated

--- Backend/routes/dashboard.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import authmiddleware from '../middleware/authmiddleware.js';
-import { getsummary } from '../controller/dashboardController.js';
-const router = express.Router();
-router.get('/summary', authmiddleware, getsummary);
-export default router;
